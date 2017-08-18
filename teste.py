d2ece4aebce21e38047f609070422ad4d84a0734
oliveirarene <31140972+oliveirarene@users.noreply.github.com>
2017-08-18 16:10:32 -0300

    Projeto Criado

---
+++ teste.py
@@ -0,0 +1,145 @@
+"""
+adicao = 72 + 23
+subtracao = 108 - 204
+multiplicacao = 108 * 0.5
+divisao = 108 / 9
+exponenciacao = 2 ** 3
+"""
+
+"""refeicao = 42.54
+taxa = 1.1
+total = refeicao * taxa
+
+print("R$ %.4f" % total)
+"""
+
+
+
+
+"""nome = "Renê"
+
+print(nome[0])
+print(nome[1])
+print(nome[2])
+print(nome[3])
+"""
+
+"""
+
+nome = "renê"
+print(nome.lower())
+print(nome.capitalize())
+"""
+"""
+nome = "renê"
+print(len(nome))
+
+print(nome[len(nome) - 1])
+"""
+
+"""numero = 10
+print(numero)
+print("numero: " + str(numero))"""
+
+"""nome = "Renê"
+sobrenome = "Oliveira"
+
+print(nome + " " + sobrenome)
+
+
+print((nome + " " + sobrenome).upper()"""
+
+"""print(str(5) + " anos")"""
+
+"""nome = "Renê"
+sobrenome = "Oliveira"
+print("{1} \n{0}".format(sobrenome, nome))"""
+
+"""refeicao = 42.54
+taxa = 1.1
+total = refeicao * taxa
+
+print("R$ %.2f" % total)
+print("R${:+07.3f}".format(total))"""
+
+"""numero = 1
+moeda = "R$"
+
+print(moeda + str(numero))
+
+print("{} {:.2f}".format(moeda, numero))"""
+
+"""a = 3
+b = 5
+c = 2
+
+r = a / b * c
+print("{:.2f}".format(r))"""
+
+"""nome = "Renê"
+print(nome[0])
+print(nome[1])
+print(nome[2])
+print(nome[3])"""
+
+"""nome = "Renê"
+print(len(nome))"""
+
+"""
+3 200
+x  1600
+ 200x = 3*1600 
+ x = 3*1600 \ 200 
+  """
+
+"""a = 3
+b = 1600
+c = 200
+resultado = 3 * 1600 / 200
+
+print("São necessários " + str(resultado) + " caminhões...")
+print("São necessários {:.0f} caminhões...".format(resultado))"""
+
+""" 1 catap 6 baterias 15 min cada = 300 pedras
+            10          12               x
+            
+            90 = 300
+            120 = x
+            
+            90x = 300 * 120
+            x=300 * 120 / 90
+
+"""
+"""quantidadeBaterias = 10
+tempoBateria = 20
+
+resultado = 300 * (quantidadeBaterias * tempoBateria) / (6 * 15)
+print("A catapulta lançará {:.0f} pedras em {} baterias de {} minutos".format(resultado, quantidadeBaterias, tempoBateria))"""
+
+
+"""75=300
+56=x
+
+75 x = 56 * 300
+x= 16800 / 56
+x = 300"""
+
+"""minutos = 2
+baterias = 100
+
+
+print("A catapulta lançará {:.0f} pedras em {} baterias de {} minutos cada.".format(resultado, baterias, minutos))"""
+
+
+"""nome = input("Digite seu nome: ")
+sobrenome = input("Digite seu sobrenome: ")
+print("{} {}".format(nome, sobrenome))"""
+
+
+
+"""minutos = int(input("Quantos minutos "))
+baterias = int(input("Quantos baterias "))
+resultado = int(minutos * int(baterias) * 300) / (15 * 5)
+print("A catapulta lançará {:.0f} pedras em {} baterias de {} minutos cada.".format(resultado, baterias, minutos))"""
+
+
